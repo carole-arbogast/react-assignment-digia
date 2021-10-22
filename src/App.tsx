b44d7270992ad25c4c3bcd02a5c3eccd90ff4634
Carole Arbogast <arbogast.carole@gmail.com>
2021-10-22 17:39:09 +0300

    feat: add GlobalStyle to style body

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Header from "./components/Header";
+import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle />
+
+      <Header />
+      <Container>
+        <Title>List of participants</Title>
+      </Container>
     </div>
   );
 }
+
+const Container = styled.div`
+  max-width: 912px;
+  margin: 2rem;
+`;
+
+const Title = styled.h2`
+  color: #757575;
+`;
 
 export default App;
